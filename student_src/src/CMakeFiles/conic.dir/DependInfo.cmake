19f759abcad111d1a60dca19290c879d0550531d
saragrass <124997838+saragrass@users.noreply.github.com>
2023-11-17 14:36:55 +0100

    Add files via upload

---
+++ student_src/src/CMakeFiles/conic.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Sara/IMAC/Cours IMAC/Projet Prog Objet/student_src/student_src/src/Directory.cpp" "CMakeFiles/conic.dir/Directory.cpp.o" "gcc" "CMakeFiles/conic.dir/Directory.cpp.o.d"
+  "/mnt/c/Sara/IMAC/Cours IMAC/Projet Prog Objet/student_src/student_src/src/Entry.cpp" "CMakeFiles/conic.dir/Entry.cpp.o" "gcc" "CMakeFiles/conic.dir/Entry.cpp.o.d"
+  "/mnt/c/Sara/IMAC/Cours IMAC/Projet Prog Objet/student_src/student_src/src/Geogebra_conics.cpp" "CMakeFiles/conic.dir/Geogebra_conics.cpp.o" "gcc" "CMakeFiles/conic.dir/Geogebra_conics.cpp.o.d"
+  "/mnt/c/Sara/IMAC/Cours IMAC/Projet Prog Objet/student_src/student_src/src/conics.cpp" "CMakeFiles/conic.dir/conics.cpp.o" "gcc" "CMakeFiles/conic.dir/conics.cpp.o.d"
+  "/mnt/c/Sara/IMAC/Cours IMAC/Projet Prog Objet/student_src/student_src/src/main.cpp" "CMakeFiles/conic.dir/main.cpp.o" "gcc" "CMakeFiles/conic.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ student_src/src/CMakeFiles/conic.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/conic.dir/Directory.cpp.o"
+  "CMakeFiles/conic.dir/Directory.cpp.o.d"
+  "CMakeFiles/conic.dir/Entry.cpp.o"
+  "CMakeFiles/conic.dir/Entry.cpp.o.d"
+  "CMakeFiles/conic.dir/Geogebra_conics.cpp.o"
+  "CMakeFiles/conic.dir/Geogebra_conics.cpp.o.d"
+  "CMakeFiles/conic.dir/conics.cpp.o"
+  "CMakeFiles/conic.dir/conics.cpp.o.d"
+  "CMakeFiles/conic.dir/main.cpp.o"
+  "CMakeFiles/conic.dir/main.cpp.o.d"
+  "conic"
+  "conic.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/conic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
