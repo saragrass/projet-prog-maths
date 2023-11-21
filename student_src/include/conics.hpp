c23577a1245587df26fa8df15bdbadc3f808d742
saragrass <124997838+saragrass@users.noreply.github.com>
2023-11-22 00:53:45 +0100

    Update conics.hpp
    il reste à réorganiser mieux le code et ajouter des commentaires

--- student_src/include/conics.hpp
+++ student_src/include/conics.hpp
@@ -4,17 +4,42 @@
 #include <Eigen/Dense>
 #include "Geogebra_conics.hpp"
 
-class Conic {
-public:
-    Conic(const std::string& filename); // constructor based on a file containing all the points
+namespace MyConics {
+    class Conic {
+        public:
+            Conic();
 
-    Eigen::VectorXd solveLeastSquares();
+            Conic(const std::string& filename); // constructor based on a file containing all the points
 
-    void printCoefficients(const Eigen::VectorXd& coefficients);
+            Eigen::VectorXd solveLeastSquares() const;
 
-    void drawConic(const Eigen::VectorXd& coefficients, Viewer_conic& viewer);
+            void printCoefficients(const Eigen::VectorXd& coefficients) const;
 
-private:
-    std::string filename_;
-    std::vector<Eigen::VectorXd> points_;
-};
+            void drawConic(const Eigen::VectorXd& coefficients, Viewer_conic& viewer, const unsigned int &red = 0, const unsigned int &green = 0, const unsigned int &blue = 200) const;
+
+            void displayConic() const;
+
+        private:
+            void readPointsFromFile();
+            void analyseLine(const std::string& line);
+            Eigen::MatrixXd buildMatrixA() const;
+            Eigen::VectorXd SVDMethod(const Eigen::MatrixXd& A) const;
+
+        private:
+            std::string filename_;
+            std::vector<Eigen::VectorXd> points_;
+    };
+    
+    class ConicBundle : public Conic {
+        public:
+            ConicBundle(const std::string& filenameA, const std::string& filenameB);
+
+            Eigen::VectorXd generateConicFromBundle(double t) const;
+
+            void displayConicBundle(double tStep = 0.1) const;
+
+        private:
+            Conic conicA_;
+            Conic conicB_;
+    };
+}
