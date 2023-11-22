b6906dadc4e345e36b8fe9dea06ce74b94a75bf5
saragrass <124997838+saragrass@users.noreply.github.com>
2023-11-22 18:58:25 +0100

    Update main.cpp

--- student_src/src/main.cpp
+++ student_src/src/main.cpp
@@ -2,19 +2,21 @@
 #include <vector>
 
 #include "conics.hpp"
+
 using namespace MyConics;
 
-// g++ -Wall -O2 -std=c++11 -I /usr/include/eigen3 main.cpp Geogebra_conics.cpp Entry.cpp Directory.cpp conics.cpp -o conics
+// g++ -Wall -O2 -std=c++11 -I /usr/include/eigen3 -I include src/main.cpp src/Geogebra_conics.cpp src/Entry.cpp src/Directory.cpp src/conics.cpp -o conics
 
 int main() {
-  Conic conic("points_hyperbola2.txt");
-  /*name of the file were are defined the coordinates of the 5 points (xi, yi), you can choose between:
+  /*name of the files where are defined the coordinates of the 5 points (xi, yi), you can choose between:
   - points_circle.txt: a point on the unit circle: x = cos(theta) and y = sin(theta)
   - points_ellipse.txt: a point on an ellipse: x = a*cos(theta) and y = b*sin(theta) with a = semi-major axe and b = semi-minor axe
   - points_parabola.txt: a point on a parabola: ax² + bx + c = y, here a = 1, b = 2 et c = 3
   - points_hyperbola.txt: a point on a hyperbola: constant/x = y, here constant = 3
   - points_hyperbola2.txt: hyperbola with non-aligned points
   */
+
+  Conic conic("points_hyperbola2.txt");
   conic.displayConic();
 
   ConicBundle conicBundle("points_hyperbola2.txt", "points_parabola.txt");
