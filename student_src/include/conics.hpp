3b4acfe09e85eb8a5617195d428d1dcbaa05a138
saragrass <124997838+saragrass@users.noreply.github.com>
2023-11-23 23:10:20 +0100

    ajout oubli conics.hpp

--- student_src/include/conics.hpp
+++ student_src/include/conics.hpp
@@ -1,46 +1,148 @@
-#pragma once
+#include <cmath>
+#include <fstream> // std::ifstream
+#include <sstream> // std::istringstream
 
-#include <vector>
-#include <Eigen/Dense>
+#include "conics.hpp"
 
-#include "Geogebra_conics.hpp"
+using namespace MyConics;
 
-namespace MyConics {
-    class Conic {
-        private:
-            std::string m_filename_;
-            std::vector<Eigen::VectorXd> m_points_;
+double evaluateTrigExpression(const std::string &expression) {
+    // check if the expression contains "cos" or "sin"
+    if (expression.find("cos") != std::string::npos) { // if std::string::find doesn't find th expression, it returns std::string::npos
+        double angle;
+        sscanf(expression.c_str(), "cos(%lf)", &angle); // "&lf" converts the string into a double
+        return std::cos(angle * M_PI / 180.0); // converts the angle from degrees to radians
+    } else if (expression.find("sin") != std::string::npos) {
+        double angle;
+        sscanf(expression.c_str(), "sin(%lf)", &angle);
+        return std::sin(angle * M_PI / 180.0);
+    } else {
+        try {
+            return std::stod(expression);
+        } catch (const std::invalid_argument& e) {
+            std::cerr << "Invalid value or expression: " << expression << " (if it's the last component, let's change it into 1)" << std::endl;
+            return std::numeric_limits<double>::quiet_NaN(); // return NaN instead of an error, it allows to return a value anyway
+        }
+    }
+}
 
-        public:
-            Conic();
+Conic::Conic() {}; // default constructor
 
-            Conic(const std::string& filename); // constructor based on a file containing all the points
+Conic::Conic(const std::string& filename) : m_filename_(filename) { readPointsFromFile();} // constructor with a file containing points
 
-            virtual Eigen::VectorXd solveLeastSquares() const;
+Conic::~Conic() {}; // destructor
 
-            virtual void printCoefficients(const Eigen::VectorXd& coefficients) const;
+Eigen::VectorXd Conic::solveLeastSquares() const{
+    assert(m_points_.size() >= 5 && "There must be at least 5 points.");
 
-            virtual void drawConic(const Eigen::VectorXd& coefficients, Viewer_conic& viewer, const unsigned int &red = 0, const unsigned int &green = 0, const unsigned int &blue = 200) const;
+    Eigen::MatrixXd A = buildMatrixA();
+    Eigen::VectorXd coefficients = SVDMethod(A);
 
-            virtual void displayConic() const;
+    return coefficients;
+}
 
-        private:
-            void readPointsFromFile();
-            void analyseLine(const std::string& line);
-            Eigen::MatrixXd buildMatrixA() const;
-            Eigen::VectorXd SVDMethod(const Eigen::MatrixXd& A) const;
-    };
-    
-    class ConicBundle : public Conic {
-        private:
-            Conic m_conicA_;
-            Conic m_conicB_;
+void Conic::readPointsFromFile() {
+    std::ifstream file(m_filename_); // open the file with the given filename for reading
+    try {
+        if (!file.is_open()) {
+            throw std::runtime_error("Error: the file cannot be opened.");
+        }
 
-        public:
-            ConicBundle(const std::string& filenameA, const std::string& filenameB);
+        std::string line; 
+        // read each line and analyse its content
+        while (std::getline(file, line)) { // read a line from the file and store it in the string 'line'
+            analyseLine(line);
+        }
+    } catch (const std::exception &e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+        throw;
+    }
 
-            virtual Eigen::VectorXd generateConicFromBundle(double t) const;
+    file.close();
+}
 
-            virtual void displayConicBundle(double tStep = 0.1) const;
-    };
+void Conic::analyseLine(const std::string& line) {
+    std::istringstream iss(line);
+    std::string token;
+
+    Eigen::VectorXd point(3);
+    bool validLine = true;
+
+    // extract values from the line and build a 3D point
+    for (int i = 0; i < 3; ++i) {
+        if (iss >> token) {
+            double value = evaluateTrigExpression(token);
+            point(i) = value;
+        } else {
+            std::cerr << "Invalid line format: " << line << std::endl;
+            validLine = false;
+            break;
+        }
+    }
+
+    if (validLine) {
+        point(2) = std::fabs(point(2));
+        if (point(2) == 0.0) {
+            std::cout << "Infinite point detected: (" << point(0) << ", " << point(1) << ")\n";
+        } else if (std::isnan(point(2))) {
+            point(2) = 1.0;
+        } else {
+            std::cout << "Regular point: (" << point(0) << ", " << point(1) << ", " << point(2) << ")\n";
+            m_points_.push_back(point);
+        }
+    }
+}
+
+Eigen::MatrixXd Conic::buildMatrixA() const {
+    Eigen::MatrixXd A(m_points_.size(), 6);
+    // fill matrix A based on the points
+    for (size_t i = 0; i < m_points_.size(); i++) {
+        Eigen::VectorXd pt = m_points_[i];
+        A(i, 0) = pt(0) * pt(0);
+        A(i, 1) = pt(0) * pt(1);
+        A(i, 2) = pt(1) * pt(1);
+        A(i, 3) = pt(0) * pt(2);
+        A(i, 4) = pt(1) * pt(2);
+        A(i, 5) = pt(2) * pt(2);
+    }
+    return A;
+}
+
+Eigen::VectorXd Conic::SVDMethod(const Eigen::MatrixXd& A) const {
+    Eigen::JacobiSVD<Eigen::MatrixXd> svd(A, Eigen::ComputeThinU | Eigen::ComputeFullV);
+    Eigen::VectorXd coefficients = svd.matrixV().rightCols(1);
+    return coefficients;
+}
+
+void Conic::printCoefficients(const Eigen::VectorXd& coefficients) const {
+    std::cout << "Coefficients de la conique avec la méthode des Moindres Carrés : " << std::endl;
+    for (int i = 0; i < coefficients.size(); ++i) {
+        std::cout << "a" << i + 1 << " = " << coefficients(i) << std::endl;
+    }
+}
+
+void Conic::drawConic(const Eigen::VectorXd& coefficients, Viewer_conic& viewer, const unsigned int &red, const unsigned int &green, const unsigned int &blue) const {
+    // draw the points in the viewer
+    for (const auto& pt : m_points_) {
+        viewer.push_point(pt, "p", 200, 0, 0);
+    }
+
+    Eigen::VectorXd conic(6);
+    // fill the conic vector with coefficients
+    conic << coefficients(0), coefficients(1), coefficients(2), coefficients(3), coefficients(4), coefficients(5);
+    // draw the conic in the viewer
+    viewer.push_conic(conic, red, green, blue);
+}
+
+void Conic::displayConic() const {
+    Eigen::VectorXd conicCoefficients = solveLeastSquares();
+
+    printCoefficients(conicCoefficients);
+
+    Viewer_conic viewer;
+
+    drawConic(conicCoefficients, viewer);
+
+    viewer.display();
+    viewer.render("output.html");
 }
