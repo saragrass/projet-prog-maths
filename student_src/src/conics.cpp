24ca8dbb6a4e83ecff31775f089cf8075cd625b4
saragrass <124997838+saragrass@users.noreply.github.com>
2023-11-23 13:00:19 +0100

    séparation en deux fichiers conics.cpp
    conics.cpp + conics_bundle.cpp

--- student_src/src/conics.cpp
+++ student_src/src/conics.cpp
@@ -26,8 +26,6 @@ double evaluateTrigExpression(const std::string &expression) {
     }
 }
 
-/////////////////////////////////////////////////// conic
-
 Conic::Conic() {}; // default constructor
 
 Conic::Conic(const std::string& filename) : filename_(filename) { readPointsFromFile();} // constructor with a file containing points
@@ -139,48 +137,10 @@ void Conic::displayConic() const {
 
     printCoefficients(conicCoefficients);
 
-    Viewer_conic viewer;
+    Viewer_conic viewer;a
 
     drawConic(conicCoefficients, viewer);
 
     viewer.display();
     viewer.render("output.html");
-}
-
-/////////////////////////////////////////////////// conic bundle
-
-ConicBundle::ConicBundle(const std::string& filenameA, const std::string& filenameB) : conicA_(filenameA), conicB_(filenameB) {}
-
-Eigen::VectorXd ConicBundle::generateConicFromBundle(double t) const {
-    double cosValue = std::cos(t);
-    double sinValue = std::sin(t);
-
-    Eigen::VectorXd conic = cosValue * conicA_.solveLeastSquares() + sinValue * conicB_.solveLeastSquares();
-    
-    double norm = conic.norm();
-    conic /= norm;
-    return conic;
-}
-
-void ConicBundle::displayConicBundle(double tStep) const {
-    
-    Viewer_conic viewer;
-
-    for (double t = 0.0; t <= M_PI; t += tStep) {
-        Eigen::VectorXd conicCoefficients = generateConicFromBundle(t);
-
-        std::cout << "Conic for t = " << t << ":" << std::endl;
-        printCoefficients(conicCoefficients);
-        std::cout << std::endl;
-
-        // gradiant from red to blue
-        unsigned int red = static_cast<int>(255 * (1 - t / M_PI));
-        unsigned int green = static_cast<int>(255 * 0.5 * (1 + std::sin(t - M_PI / 6.0)));
-        unsigned int blue = static_cast<int>(255 * (t / M_PI));
-
-        drawConic(conicCoefficients, viewer, red, green, blue);
-    }
-
-    viewer.display();
-    viewer.render("output_bundle.html");
 }
