4595b23f4c9640d56aef7db0aba1934df8398795
saragrass <124997838+saragrass@users.noreply.github.com>
2023-11-23 13:00:54 +0100

    added conics_bundle.cpp

---
+++ student_src/src/conics_bundle.cpp
@@ -0,0 +1,39 @@
+#include "conics.hpp"
+
+using namespace MyConics;
+
+ConicBundle::ConicBundle(const std::string& filenameA, const std::string& filenameB) : conicA_(filenameA), conicB_(filenameB) {}
+
+Eigen::VectorXd ConicBundle::generateConicFromBundle(double t) const {
+    double cosValue = std::cos(t);
+    double sinValue = std::sin(t);
+
+    Eigen::VectorXd conic = cosValue * conicA_.solveLeastSquares() + sinValue * conicB_.solveLeastSquares();
+    
+    double norm = conic.norm();
+    conic /= norm;
+    return conic;
+}
+
+void ConicBundle::displayConicBundle(double tStep) const {
+    
+    Viewer_conic viewer;
+
+    for (double t = 0.0; t <= M_PI; t += tStep) {
+        Eigen::VectorXd conicCoefficients = generateConicFromBundle(t);
+
+        std::cout << "Conic for t = " << t << ":" << std::endl;
+        printCoefficients(conicCoefficients);
+        std::cout << std::endl;
+
+        // gradiant from red to blue
+        unsigned int red = static_cast<int>(255 * (1 - t / M_PI));
+        unsigned int green = static_cast<int>(255 * 0.5 * (1 + std::sin(t - M_PI / 6.0)));
+        unsigned int blue = static_cast<int>(255 * (t / M_PI));
+
+        drawConic(conicCoefficients, viewer, red, green, blue);
+    }
+
+    viewer.display();
+    viewer.render("output_bundle.html");
+}
